37a2e59587cac0b905a9b7a90af7c76732f73183
fuyunfei <fuyunfei1991@gmail.com>
2016-07-06 11:13:37 +0100

    success!

--- Makefile.sh
+++ Makefile.sh
@@ -1,3 +1,3 @@
 gcc -c -o propng.o propng.c -lm -lpng -lstdc++ 
-g++ -c -o pngply.o pngply.cc  -lm -lpng -lstdc++ 
-g++ -o main propng.o pngply.o -lm -lpng -lstdc++ 
+g++ -c -o pngply.o pngply.cc -I /usr/include/eigen3/  -lm -lpng -lstdc++ 
+g++ -o main propng.o pngply.o -I /usr/include/eigen3/ -lm -lpng -lstdc++ 
--- pngply.cc
+++ pngply.cc
@@ -4,16 +4,21 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Core>
 extern "C" {
 #include "propng.h"
 }
 using namespace std;
+ using namespace Eigen;
 
 int main(int argc, char **argv)
 {
-        int width =960; 
+    int width =960;
 	int height =960;
-	float focal = 430;
+	float focal = 595;
 
         png_bytep * pr;
         pr=read_png_file(argv[1]);
@@ -30,20 +35,32 @@ int main(int argc, char **argv)
  for( string line;  getline(myfile, line); )
  {
      	std::istringstream in(line);      //make a stream for the line itself
-      	float x, y, z;
+      	float x, y, z,px,py,pz;
 	int r,g,b; 
 	in>>x>>y>>z>>r>>g>>b;
-	int xj=width/2-x*focal/z;
-	int yj=height/2-y*focal/z;
-	if (xj>width||xj<0||yj>width||yj<0){
+	MatrixXf P(4,1),PRT(4,1);
+	MatrixXf RT(3,4);
+	P<<x,y,z,1.0;
+	RT<<0.999897,-0.010837,0.009367,0.189848,
+	0.010407,0.998942,0.044801,0.067492,
+	-0.009843, -0.044699 ,0.998952,-0.992342;
+	PRT=RT*P;
+	px=PRT(0,0);
+	py=PRT(1,0);
+	pz=PRT(2,0);
+	
+	int xj=width/2-px*focal/pz;
+	int yj=height/2-py*focal/pz;
+	if (xj>width-10||xj<0||yj>width-10||yj<0){
 	cout <<i<<" "<<xj<<" "<<yj<<endl;	
-	}
+	}else{
 	i++;
 	png_byte* row = pr[xj];
 	png_byte* ptr = &(row[yj*3]);
 	ptr[0] = r;
 	ptr[1] = g;
 	ptr[2] = b;
+	}
 }
 
      //   png_byte* row = row_pointers[y];
